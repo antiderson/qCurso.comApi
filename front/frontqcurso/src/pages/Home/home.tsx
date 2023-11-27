532321890319f011e9f3c38550cbd759d51edef0
Anderson <anderbreiksondaniel@gmail.com>
2023-11-27 12:07:15 -0300

    homePronta

--- front/frontqcurso/src/pages/Home/home.tsx
+++ front/frontqcurso/src/pages/Home/home.tsx
@@ -9,6 +9,15 @@ import typeScriptLogo from "../../assets/type-logo.svg"
 import bancoLogo from "../../assets/db-logo.svg"
 import javaLogo from "../../assets/java-logo.svg"
 import outroLogo from "../../assets/outro-logo.svg"
+import layout from "../../assets/layout.png"
+import syntax from "../../assets/syntax.png"
+import element from "../../assets/element.png"
+import content from "../../assets/content.png"
+import documentacao from "../../assets/documentacao.png"
+import ideia from "../../assets/ideia.png"
+import duvida from "../../assets/duvida.png"
+import colabore from "../../assets/colabore.png"
+import footer from "../../assets/rodape.png"
 
 
 export default function Home() {
@@ -39,7 +48,7 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <div style={{ paddingTop: "8%", marginBottom:"5%" }}>
+                    <div style={{ paddingTop: "8%", marginBottom: "5%" }}>
                         <section className="layout" >
                             <div className="itemTable">
                                 <div className="iconL">
@@ -124,9 +133,31 @@ export default function Home() {
                             </div>
                         </section>
                     </div>
-                    <div className="containerSeg">
+                    <div className="containerSeg justify-content-center">
                         <h1 className="text-center" >Tópicos recomendados</h1>
+                        <div className="teste d-flex flex-wrap">
+                            <img src={element} alt="" />
+                            <img src={content} alt="" />
+                            <img src={syntax} alt="" />
+                            <img src={layout} alt="" />
+                        </div>
                     </div>
+
+                    <div id="teste" className="justify-content-center">
+                        <h3 className="text-center" >Como qCurso funciona</h3>
+                        <div className="teste2 d-flex row">
+                            <a className="nav-link text-white" href="#"><img src={duvida} alt="" /></a>
+                            <a className="nav-link text-white" href="#"><img src={ideia} alt="" /></a>
+                            <a className="nav-link text-white" href="#"><img src={documentacao} alt="" /></a>
+                        </div>
+                    </div>
+                    <div className=" d-flex justify-content-center">
+                        <img className="colabore" src={colabore} alt="" />
+                    </div>
+                    <hr />
+                    <footer >
+                        <img className="footer" src={footer} alt="" />
+                    </footer>
                 </div>
             </div>
         </>
